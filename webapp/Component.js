f253e0085b0229437d3a4e54087efe8f5a0a1b94
Adriana Dias <adias.adriana@gmail.com>
2023-01-22 18:04:57 -0300

    commit inicial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns.recrutamentoalp2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
